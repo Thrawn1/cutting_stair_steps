04ff5803ae8c87b73d4705891ee98f1b39c4320c
Thrawn1 <b.eremin@gmail.com>
2024-07-21 18:21:36 +0300

    Update MashineLimits.py

--- MashineLimits.py
+++ MashineLimits.py
@@ -1,16 +1,17 @@
-class MachineLimits():
+class MachineLimits:
+    """
+    Класс описывающий ограничения станка
 
-    """Класс описывающий ограничения станка
     Параметры:
     - x_min (float): минимальное значение координаты X
     - x_max (float): максимальное значение координаты X
     - y_min (float): минимальное значение координаты Y
     - y_max (float): максимальное значение координаты Y
-    - type_mashine (float): модель станка
+    - model_machine (str): модель станка
     """
 
     def __init__(self, x_min: float, x_max: float, y_min: float,
-                  y_max: float, model_machine: str) -> None:
+                 y_max: float, model_machine: str) -> None:
         if x_min >= x_max:
             raise ValueError("x_min should be less than x_max")
         if y_min >= y_max:
@@ -19,7 +20,7 @@ class MachineLimits():
         self.x_max = x_max
         self.y_min = y_min
         self.y_max = y_max
-        self.type_machine = model_machine
+        self.model_machine = model_machine
 
     def check_coordinate(self, axis: str, value: float) -> float:
         """
