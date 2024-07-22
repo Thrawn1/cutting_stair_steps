9d888c304afde6539addc4f36e7d77e9df3dd148
Thrawn1 <b.eremin@gmail.com>
2024-07-22 14:16:29 +0300

    Create GeneratorGcode.py

---
+++ GeneratorGcode.py
@@ -0,0 +1,7 @@
+class GeneratorGcode():
+    """Класс для генерации g-code"""
+    def __init__(self, worker,parameters):
+        self.worker = worker
+        self.parameters = parameters
+    def generate_gcode(self):
+        pass
