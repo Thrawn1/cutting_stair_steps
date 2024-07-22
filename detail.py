b40ae30ae9c1fb67bdf7cdeae1a45df536d38a19
Thrawn1 <b.eremin@gmail.com>
2024-07-22 12:34:31 +0300

    Update detail.py

--- detail.py
+++ detail.py
@@ -11,12 +11,36 @@ class Detail():
         - material (str): материал детали
         - rectangle (bool): является ли деталь прямоугольной (по умолчанию True)
         """
+        if width <= 0 or length <= 0 or thickness <= 0:
+                    raise ValueError("Ширина, длина и толщина должны быть положительными числами.")
+        if not isinstance(width, (int, float)):
+            raise ValueError("Ширина должна быть числом.")
+        if not isinstance(length, (int, float)):
+            raise ValueError("Длина должна быть числом.")
+        if not isinstance(thickness, (int, float)):
+            raise ValueError("Толщина должна быть числом.")
+        if not isinstance(material, str):
+            raise ValueError("Материал должен быть строкой.")
+        if not isinstance(rectangle, bool):
+            raise ValueError("rectangle должен быть булевым значением.")
+        
+
         self.width = width
         self.length = length
         self.thickness = thickness
         self.material = material
         self.rectangle = rectangle
-        self.area = width * length
+        self.area = self.calculate_area()
+    
+    def calculate_area(self):
+        """
+        Рассчитывает площадь детали.
+        Возвращает:
+        - float: площадь детали
+        """
+        l = self.length/1000
+        w = self.width/1000
+        return l * w
     
     def __str__(self):
         """
@@ -25,7 +49,7 @@ class Detail():
         - str: строковое представление объекта Detail
         """
         return (f"Деталь: {self.width}x{self.length}x{self.thickness} "
-                f"из материала {self.material}")
+                f"из материала {self.material}, площадь {self.area} м^2")
     def __repr__(self):
         return self.__str__()
     
