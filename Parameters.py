a934b550916fab4c2320617c7b52cec7432eb0a3
Thrawn1 <b.eremin@gmail.com>
2024-07-23 08:23:13 +0300

    Update Parameters.py

--- Parameters.py
+++ Parameters.py
@@ -67,51 +67,31 @@ class DepthParameters:
 
 
 
-
-
 class Parameters:
     """
     Класс для хранения информации о параметрах резки:
-    - speed_forward (int): скорость прямого хода резки
-    - speed_backward (int): скорость обратного хода резки (по умолчанию 0.7 от скорости прямого хода)
-    - speed_depth (int): скорость заглубления инструмента
-    - depth_step_forward (float): шаг глубины работы инструмента по прямому ходу
-    - depth_step_backward (float): шаг глубины работы инструмента по обратному ходу (по умолчанию равен шагу глубины работы инструмента по прямому ходу)
+    - speed_parameters (SpeedParameters): параметры скорости резки
+    - depth_parameters (DepthParameters): параметры глубины резки
     - zigzag (bool): флаг режима зиг-заг (по умолчанию True)
-    - extra_depth (int): дополнительная глубина реза (по умолчанию 0)
+
     """
     
-    def __init__(self, speed_forward:int, speed_depth:int, depth_step_forward:float,
-                 speed_backward=None, depth_step_backward=None, zigzag=True,extra_depth=0):
+    def __init__(self, speed_parameters:SpeedParameters,
+                 depth_parameters:DepthParameters,zigzag=True) -> None:
         """
         Параметры:
-        - speed_forward (int): скорость прямого хода резки
-        - speed_backward (int): скорость обратного хода резки (по умолчанию 0.7 от скорости прямого хода)
-        - speed_depth (int): скорость заглубления инструмента
-        - depth_step_forward (float): шаг глубины работы инструмента по прямому ходу
-        - depth_step_backward (float): шаг глубины работы инструмента по обратному ходу (по умолчанию равен шагу глубины работы инструмента по прямому ходу)
+        - speed_parameters (SpeedParameters): параметры скорости резки
+        - depth_parameters (DepthParameters): параметры глубины резки
         - zigzag (bool): флаг режима зиг-заг (по умолчанию True)
-        - extra_depth (int): дополнительная глубина реза (по умолчанию 0)
         """
 
-        self.validate_type(speed_forward, "speed_forward", int)
-        self.validate_type(speed_depth, "speed_depth", int)
-        self.validate_type(depth_step_forward, "depth_step_forward", float)
-        if speed_backward is not None:
-            self.validate_type(speed_backward, "speed_backward", int)
-        if depth_step_backward is not None:
-            self.validate_type(depth_step_backward, "depth_step_backward", float)
+        self.validate_type(speed_parameters, "speed_parameters", SpeedParameters)
+        self.validate_type(depth_parameters, "depth_parameters", DepthParameters)
         self.validate_type(zigzag, "zigzag", bool)
-        self.validate_type(extra_depth, "extra_depth", int)
 
-
-        self.speed_forward = speed_forward
-        self.speed_backward = speed_backward if speed_backward is not None else 0.7 * speed_forward
-        self.speed_depth = speed_depth
-        self.depth_step_forward = depth_step_forward
-        self.depth_step_backward = depth_step_backward if depth_step_backward is not None else depth_step_forward
+        self.speed_parameters = speed_parameters
+        self.depth_parameters = depth_parameters        
         self.zigzag = zigzag
-        self.extra_depth = extra_depth
 
     @staticmethod
     def validate_type(value, name, type_):
