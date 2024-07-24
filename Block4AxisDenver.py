e8f36a0eb06314000c8e4c1ebf5f07a87c7ef815
Thrawn1 <b.eremin@gmail.com>
2024-07-24 22:23:29 +0300

    Update Block4AxisDenver.py

--- Block4AxisDenver.py
+++ Block4AxisDenver.py
@@ -2,135 +2,86 @@ from Disk import Disk
 from Worker import Worker
 from Parameters import Parameters
 
-class Block4AxisDenver():
+class Block4AxisDenver:
     """Класс для генерации блоков кода для станка Denver с 4-мя осями"""
-    def __init__(self,name_programm,disk:Disk,worker:Worker,parametrs:Parameters,count_line=1):
+    
+    def __init__(self, name_programm, disk: Disk, worker: Worker, parameters: Parameters, count_line=1):
         self.count_line = count_line
         self.name_programm = name_programm
         self.disk = disk 
         self.worker = worker
-        self.parametrs = parametrs
+        self.parameters = parameters
+
+    @staticmethod
+    def get_coordinates_axis_c(key):
+        """Получение координаты по оси С"""
+        if key == 'X1X2Y':
+            return 0
+        elif key == 'Y1Y2X':
+            return 90
+        else:
+            raise ValueError("Неверный ключ")
+
 
     def start_block(self):
-        start_blok = []
-        line = f";({self.name_programm})"
-        start_blok.append(line)
-        line = f"N{self.count_line} (UAO,1)"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} G331 S60"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} (UIO,Z(E31))"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f";{self.disk.name} [D. {self.disk.diameter} ] D. {self.disk.diameter}"
-        start_blok.append(line)
-        line = f"N{self.count_line} G398"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} #G0 C0"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} T{self.disk.number}"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} M52"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} G376"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} G17"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} G317 P12"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} L365=3"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} M41"
-        self.count_line += 1
-        start_blok.append(line)
-        line = ";(MMSystem)"
-        start_blok.append(line)
-        line = f"N{self.count_line} M44"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} G397 A0"
-        self.count_line += 1
-        start_blok.append(line)
-        line = f"N{self.count_line} M41"
-        self.count_line += 1
-        start_blok.append(line)
-        self.start_block = start_blok
-    def end_block(self):
-        end_blok = []
-        line = f"N{self.count_line} G00 Z{self.disk.Z_sec}"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} M44"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} M05"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} M09 M10"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} G398"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} #G0 G79 X0 Y0 C0"
-        self.count_line += 1
-        end_blok.append(line)
-        line = f"N{self.count_line} M30"
-        self.count_line += 1
-        end_blok.append(line)
-        line = '\n'
-        end_blok.append(line)
-        self.end_block = end_blok
+        """Генерация стартового блока команд"""
+        start_move_key = self.worker.get_start_move_key()
+        start_block = [
+            f";({self.name_programm})",
+            f"N{self.count_line} (UAO,1)", 
+            f"N{self.count_line + 1} G331 S60",
+            f"N{self.count_line + 2} (UIO,Z(E31))",
+            f";{self.disk.name} [D. {self.disk.diameter} ] D. {self.disk.diameter}",
+            f"N{self.count_line + 3} G398",
+            f"N{self.count_line + 4} #G0 C{self.get_coordinates_axis_c(start_move_key)}",
+            f"N{self.count_line + 5} T{self.disk.number}",
+            f"N{self.count_line + 6} M52",
+            f"N{self.count_line + 7} G376",
+            f"N{self.count_line + 8} G17",
+            f"N{self.count_line + 9} G317 P12",
+            f"N{self.count_line + 10} L365=3",
+            f"N{self.count_line + 11} M41"
+        ]
+        self.count_line += len(start_block)
+        return start_block
 
-    def work_blok(self):
-        work_blok = []
-        line_start = f'N{self.count_line} M140[1]'
-        work_blok.append(line_start)
+    def work_block(self):
+        """Генерация рабочего блока команд"""
+        work_block = [f'N{self.count_line} M140[1]']
         self.count_line += 1
+        
         line_end = f'N{self.count_line} M140[0]'
-        f_speed = self.parametrs.speed_forward
-        b_speed = self.parametrs.speed_backward
-        zf_speed = self.parametrs.speed_depth
-        all_coordinate_xy = self.worker.calculate_coordinates()[0]
-        all_coordinate_z = self.worker.calculate_coordinates()[1]
+        self.count_line += 1
+        
+        f_speed = self.parameters.speed_forward
+        b_speed = self.parameters.speed_backward
+        zf_speed = self.parameters.speed_depth
+        all_coordinate_xy, all_coordinate_z = self.worker.calculate_coordinates()
+
         for coordinate in all_coordinate_xy:
-            count = 1
-            for z in all_coordinate_z:
+            for count, z in enumerate(all_coordinate_z, start=1):
                 if count % 2 != 0:
-                    line = f'N{self.count_line} X{coordinate[0]} F{f_speed}'
+                    work_block.append(f'N{self.count_line} X{coordinate[0]} F{f_speed}')
                     self.count_line += 1
-                    work_blok.append(line)
-                    line = f'N{self.count_line} X{coordinate[0]} Z{z} F{zf_speed}'
-                    self.count_line += 1
-                    work_blok.append(line)
+                    work_block.append(f'N{self.count_line} X{coordinate[0]} Z{z} F{zf_speed}')
                 else:
-                    line = f'N{self.count_line} X{coordinate[1]} F{b_speed}'
+                    work_block.append(f'N{self.count_line} X{coordinate[1]} F{b_speed}')
                     self.count_line += 1
-                    work_blok.append(line)
-                    line = f'N{self.count_line} X{coordinate[1]} Z{z} F{zf_speed}'
-                    self.count_line += 1
-                    work_blok.append(line)
-        work_blok.append(line_end)
-        return work_blok
+                    work_block.append(f'N{self.count_line} X{coordinate[1]} Z{z} F{zf_speed}')
+                self.count_line += 1
+
+        work_block.append(line_end)
+        return work_block
 
     def moving_between_work_blok(self):
-        moving_between_work_blok = []
-        line = f'N{self.count_line} Z{self.disk.Z_sec}'
-        moving_between_work_blok.append(line)
+        """Генерация блока команд для перемещения между рабочими блоками"""
+        moving_between_work_blok = [f'N{self.count_line} Z{self.disk.Z_sec}']
         self.count_line += 1
-        line = f'N{self.count_line} G00 X0 Y0'
-
-
+        moving_between_work_blok.append(f'N{self.count_line} G00 X0 Y0')
+        self.count_line += 1
+        moving_between_work_blok.append(f'N{self.count_line} G00 Z{self.disk.Z_approach}')
+        self.count_line += 1
+        return moving_between_work_blok
 
 # """
 # N47 #G00 Z50
