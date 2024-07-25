aeeee22890071da9b5b8b52c3d1dc8be085449d9
Thrawn1 <b.eremin@gmail.com>
2024-07-25 13:51:10 +0300

    Update Validator.py

--- Validator.py
+++ Validator.py
@@ -1,3 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 class Validator:
     """
     Класс для валидации данных
@@ -17,6 +21,19 @@ class Validator:
             self.errors.append(f'{field_name} is empty')
         return not self.errors
 
+    def validate_min_max(self, min_value, max_value):
+        """
+        Проверяет, что минимальное значение меньше максимального
+        :param min_value: минимальное значение
+        :param max_value: максимальное значение
+        :param field_name: название поля
+        :return: True, если минимальное значение меньше максимального, иначе False
+        """
+        if not min_value < max_value:
+            self.errors.append(f'Минимальное значение больше максимального.',
+                                f'Минимальное значение: {min_value}', 
+                                f'Максимальное значение: {max_value}')
+
     def validate_type(self, data, expected_type, field_name):
         """
         Проверяет, что данные имеют ожидаемый тип
@@ -29,17 +46,29 @@ class Validator:
             self.errors.append(f'{field_name} is not of type {expected_type.__name__}')
         return not self.errors
 
-    def validate_length(self, data, min_length, max_length, field_name):
+    def validate_input (self, data, expected_type, field_name):
         """
-        Проверяет, что длина данных находится в заданных пределах
+        Проверяет, что данные имеют ожидаемый тип
         :param data: данные для проверки
-        :param min_length: минимальная длина данных
-        :param max_length: максимальная длина данных
+        :param expected_type: ожидаемый тип данных
         :param field_name: название поля
-        :return: True, если длина данных находится в заданных пределах, иначе False
+        :return: True, если данные имеют ожидаемый тип, иначе False
         """
-        if not (min_length <= len(data) <= max_length):
-            self.errors.append(f'{field_name} length is not between {min_length} and {max_length}')
+        try:
+            data = expected_type(data)
+        except ValueError:
+            self.errors.append(f'{field_name} is not of type {expected_type.__name__}')
+        return not self.errors
+
+    def checking_value_positive(self, data, field_name):
+        """
+        Проверяет, что данные положительные
+        :param data: данные для проверки
+        :param field_name: название поля
+        :return: True, если данные положительные, иначе False
+        """
+        if not data > 0:
+            self.errors.append(f'{field_name} is not positive')
         return not self.errors
 
     def get_errors(self):
@@ -54,6 +83,15 @@ class Validator:
         Очищает список ошибок
         """
         self.errors = []
+    
+    def save_to_file(self, file_name):
+        """
+        Сохраняет список ошибок в файл
+        :param file_name: имя файла
+        """
+        with open(file_name, 'w') as file:
+            for error in self.errors:
+                file.write(error + '\n')
 
     def __str__(self) -> str:
         """
