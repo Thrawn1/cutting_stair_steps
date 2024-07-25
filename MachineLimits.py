a2cb34b705db6a6f0f5c4c4bc1d1f12146a96e1a
Thrawn1 <b.eremin@gmail.com>
2024-07-25 13:51:16 +0300

    Update MachineLimits.py

--- MachineLimits.py
+++ MachineLimits.py
@@ -13,16 +13,18 @@ class MachineLimits:
     """
 
     def __init__(self, x_min: float, x_max: float, y_min: float,
-                 y_max: float, model_machine: str) -> None:
-        if x_min >= x_max:
-            raise ValueError("x_min should be less than x_max")
-        if y_min >= y_max:
-            raise ValueError("y_min should be less than y_max")
-        self.x_min = x_min
-        self.x_max = x_max
-        self.y_min = y_min
-        self.y_max = y_max
-        self.model_machine = model_machine
+                 y_max: float, model_machine: str,validator:Validator) -> None:
+        if validator.validate_type(x_min, float, 'x_min') and \
+            validator.validate_type(x_max, float, 'x_max') and \
+            validator.validate_type(y_min, float, 'y_min') and \
+            validator.validate_type(y_max, float, 'y_max') and \
+            validator.validate_type(model_machine, str, 'model_machine'):
+            if validator.validate_min_max(x_min, x_max):
+                self.x_min = x_min
+                self.x_max = x_max
+                self.y_min = y_min
+                self.y_max = y_max
+                self.model_machine = model_machine
 
     def check_coordinate_axis_x_y(self, axis: str, value: float) -> float:
         """
@@ -32,12 +34,14 @@ class MachineLimits:
         :param value: Значение координаты.
         :return: Скорректированное значение координаты.
         """
-        if axis == 'X':
-            return max(self.x_min, min(value, self.x_max))
-        elif axis == 'Y':
-            return max(self.y_min, min(value, self.y_max))
-        else:
-            raise ValueError("Axis must be 'X' or 'Y'")
+        if Validator().validate_type(axis, str, 'axis') and \
+            Validator().validate_type(value, float, 'value'):
+            if axis == 'X':
+                return max(self.x_min, min(value, self.x_max))
+            elif axis == 'Y':
+                return max(self.y_min, min(value, self.y_max))
+            else:
+                raise ValueError("Axis must be 'X' or 'Y'")
     def check_coordinate_axis_c(self, value: float,type_mashine: str) -> bool:
         """
         Проверяет, соответствует ли значение координаты оси C ограничениям станка.
@@ -47,5 +51,9 @@ class MachineLimits:
         Возвращает:
         - bool: True, если значение соответствует ограничениям, иначе False
         """
-        if type_mashine == '4Axis' and value >= 0 and value <= 180:
-            return True
+        if Validator().validate_type(value, float, 'value') and \
+            Validator().validate_type(type_mashine, str, 'type_mashine'):
+            if type_mashine == '4Axis' and value >= 0.0 and value <= 180.0:
+                return True
+            elif type_mashine == '5Axis' and value >= -4.9 and value <= 360.0:
+                return True
